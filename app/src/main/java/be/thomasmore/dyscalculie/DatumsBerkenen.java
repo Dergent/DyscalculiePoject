d392b64e77b38bc45232988ebbce9bb3a633f375
Pieter-Jan Dergent <pieterjan.dergent@gmail.com>
2017-12-10 16:18:19 +0100

    datumsberekenen fix

--- app/src/main/java/be/thomasmore/dyscalculie/DatumsBerkenen.java
+++ app/src/main/java/be/thomasmore/dyscalculie/DatumsBerkenen.java
@@ -32,60 +32,70 @@ public class DatumsBerkenen extends Fragment {
         DatePicker eindDatum = getView().findViewById(R.id.eindDatum);
         TextView textView = getView().findViewById(R.id.resultaat);
 
-        if (beginDatum.getYear() < eindDatum.getYear()){
+        if (beginDatum.getYear() <= eindDatum.getYear()){
             int maand = 0;
             int dag = 0;
 
             int jaar = eindDatum.getYear() - beginDatum.getYear();
 
-            if (eindDatum.getDayOfMonth() >= beginDatum.getDayOfMonth() && eindDatum.getMonth() >= beginDatum.getMonth()) {
-                dag = eindDatum.getDayOfMonth() - beginDatum.getDayOfMonth();
-                maand = eindDatum.getMonth() - beginDatum.getMonth();
-            }
-            else {
-                if (eindDatum.getDayOfMonth() < beginDatum.getDayOfMonth()){
-                    if (eindDatum.getMonth() > beginDatum.getMonth()){
-                        maand = eindDatum.getMonth() - beginDatum.getMonth() - 1;
-                    }
-                    if (beginDatum.getMonth() == 0 || beginDatum.getMonth() == 2 || beginDatum.getMonth() == 4 || beginDatum.getMonth() == 6 || beginDatum.getMonth() == 7 || beginDatum.getMonth() == 9 || beginDatum.getMonth() == 11) {
-                        dag = 31 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
-                    }
-                    if (beginDatum.getMonth() == 1 && beginDatum.getYear()%4 == 0){
-                        dag = 29 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
-                    }
-                    if (beginDatum.getMonth() == 1 && beginDatum.getYear()%4 != 0) {
-                        dag = 28 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
-                    }
-                    if (beginDatum.getMonth() == 3 || beginDatum.getMonth() == 5 || beginDatum.getMonth() == 8 || beginDatum.getMonth() == 10){
-                        dag = 30 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
-                    }
+            if (beginDatum.getYear() == eindDatum.getYear() && (beginDatum.getMonth() > eindDatum.getMonth() || beginDatum.getDayOfMonth() > eindDatum.getDayOfMonth())){
+                textView.setText("De begindatum is voor de einddatum.");
+            } else {
+                if (eindDatum.getDayOfMonth() >= beginDatum.getDayOfMonth() && eindDatum.getMonth() >= beginDatum.getMonth()) {
+                    dag = eindDatum.getDayOfMonth() - beginDatum.getDayOfMonth();
+                    maand = eindDatum.getMonth() - beginDatum.getMonth();
                 }
-                if (eindDatum.getMonth() < beginDatum.getMonth()) {
-                    maand = 12 - beginDatum.getMonth() + eindDatum.getMonth();
-                    if (jaar != 0) {
-                        jaar -= 1;
-                    }
-                    if (eindDatum.getDayOfMonth() < beginDatum.getDayOfMonth()) {
-                        if (beginDatum.getMonth() == 1 || beginDatum.getMonth() == 3 || beginDatum.getMonth() == 5 || beginDatum.getMonth() == 7 || beginDatum.getMonth() == 8 || beginDatum.getMonth() == 10 || beginDatum.getMonth() == 12) {
+                else {
+                    if (eindDatum.getDayOfMonth() < beginDatum.getDayOfMonth()){
+                        if (eindDatum.getMonth() > beginDatum.getMonth()){
+                            maand = eindDatum.getMonth() - beginDatum.getMonth() - 1;
+                        }
+                        if (beginDatum.getMonth() == 0 || beginDatum.getMonth() == 2 || beginDatum.getMonth() == 4 || beginDatum.getMonth() == 6 || beginDatum.getMonth() == 7 || beginDatum.getMonth() == 9 || beginDatum.getMonth() == 11) {
                             dag = 31 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
                         }
-                        if (beginDatum.getMonth() == 2 && beginDatum.getYear() % 4 == 0) {
+                        if (beginDatum.getMonth() == 1 && beginDatum.getYear()%4 == 0){
                             dag = 29 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
                         }
-                        if (beginDatum.getMonth() == 2 && beginDatum.getYear() % 4 != 0) {
+                        if (beginDatum.getMonth() == 1 && beginDatum.getYear()%4 != 0) {
                             dag = 28 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
                         }
-                        if (beginDatum.getMonth() == 4 || beginDatum.getMonth() == 6 || beginDatum.getMonth() == 9 || beginDatum.getMonth() == 11) {
+                        if (beginDatum.getMonth() == 3 || beginDatum.getMonth() == 5 || beginDatum.getMonth() == 8 || beginDatum.getMonth() == 10){
                             dag = 30 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
                         }
-                        maand -= 1;
                     }
-                    if (eindDatum.getDayOfMonth() >= beginDatum.getDayOfMonth()) {
-                        dag = eindDatum.getDayOfMonth() - beginDatum.getDayOfMonth();
+                    if (eindDatum.getMonth() < beginDatum.getMonth()) {
+                        maand = 12 - beginDatum.getMonth() + eindDatum.getMonth();
+                        if (jaar != 0) {
+                            jaar -= 1;
+                        }
+                        if (eindDatum.getDayOfMonth() < beginDatum.getDayOfMonth()) {
+                            if (beginDatum.getMonth() == 1 || beginDatum.getMonth() == 3 || beginDatum.getMonth() == 5 || beginDatum.getMonth() == 7 || beginDatum.getMonth() == 8 || beginDatum.getMonth() == 10 || beginDatum.getMonth() == 12) {
+                                dag = 31 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
+                            }
+                            if (beginDatum.getMonth() == 2 && beginDatum.getYear() % 4 == 0) {
+                                dag = 29 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
+                            }
+                            if (beginDatum.getMonth() == 2 && beginDatum.getYear() % 4 != 0) {
+                                dag = 28 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
+                            }
+                            if (beginDatum.getMonth() == 4 || beginDatum.getMonth() == 6 || beginDatum.getMonth() == 9 || beginDatum.getMonth() == 11) {
+                                dag = 30 - beginDatum.getDayOfMonth() + eindDatum.getDayOfMonth();
+                            }
+                            maand -= 1;
+                        }
+                        if (eindDatum.getDayOfMonth() >= beginDatum.getDayOfMonth()) {
+                            dag = eindDatum.getDayOfMonth() - beginDatum.getDayOfMonth();
+                        }
+                    }
+                    if (eindDatum.getDayOfMonth() == beginDatum.getDayOfMonth()) {
+                        dag = 0;
+                    }
+                    if (eindDatum.getMonth() == beginDatum.getMonth()) {
+                        maand = 0;
                     }
                 }
+                textView.setText("Verstreken tijd: " + dag + " dag(en) " + maand + " maand(en) " + jaar + " ja(a)r(en).");
             }
-            textView.setText("Verstreken tijd: " + dag + " dag(en) " + maand + " maand(en) " + jaar + " ja(a)r(en).");
         } else {
             textView.setText("De begindatum is voor de einddatum.");
         }
