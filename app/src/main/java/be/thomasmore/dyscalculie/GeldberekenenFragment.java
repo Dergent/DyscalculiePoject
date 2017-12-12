3403684a4028c78de121c63429f5d530f58bef86
Pieter-Jan Dergent <pieterjan.dergent@gmail.com>
2017-12-12 15:00:52 +0100

    Text to speach

--- app/src/main/java/be/thomasmore/dyscalculie/GeldberekenenFragment.java
+++ app/src/main/java/be/thomasmore/dyscalculie/GeldberekenenFragment.java
@@ -3,20 +3,53 @@ package be.thomasmore.dyscalculie;
 import android.icu.text.DecimalFormat;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class GeldberekenenFragment extends Fragment {
+
+    private TextToSpeech textToSpeech;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_geldberekenen, container, false);
         final Button button = view.findViewById(R.id.buttonCalculate);
+        final EditText teBetalen = view.findViewById(R.id.teBetalen);
+        final EditText gegeven = view.findViewById(R.id.gegevenBedrag);
+
+        textToSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status){
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(new Locale("nl_NL"));
+                }
+            }
+        });
+
+        teBetalen.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                textToSpeech.speak(teBetalen.getText().toString() + " euro", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        gegeven.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                textToSpeech.speak(gegeven.getText().toString() + " euro", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+
         button.setOnClickListener(new View.OnClickListener(){
             @RequiresApi(api = Build.VERSION_CODES.N)
             public void onClick(View v){
@@ -31,7 +64,7 @@ public class GeldberekenenFragment extends Fragment {
         TextView tTeBetalen = getView().findViewById(R.id.teBetalen);
         TextView tGegevenBedrag = getView().findViewById(R.id.gegevenBedrag);
         TextView tWisselgeld = getView().findViewById(R.id.wisselgeld);
-        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
 
         double teBetalen = Double.parseDouble(tTeBetalen.getText().toString());
         double gegevenBedrag = Double.parseDouble(tGegevenBedrag.getText().toString());
@@ -39,10 +72,12 @@ public class GeldberekenenFragment extends Fragment {
             double wisselgeld = gegevenBedrag - teBetalen;
 
             tWisselgeld.setText(decimalFormat.format(wisselgeld));
+            textToSpeech.speak(decimalFormat.format(wisselgeld) + " euro", TextToSpeech.QUEUE_FLUSH, null);
         } else {
             double geldTeKort = teBetalen - gegevenBedrag;
             String teKort = "Je komt: " + decimalFormat.format(geldTeKort) + " euro te kort!";
             tWisselgeld.setText(teKort);
+            textToSpeech.speak(teKort + " euro", TextToSpeech.QUEUE_FLUSH, null);
         }
     }
 }
--- app/src/main/java/be/thomasmore/dyscalculie/PercentenFragment.java
+++ app/src/main/java/be/thomasmore/dyscalculie/PercentenFragment.java
@@ -3,21 +3,51 @@ package be.thomasmore.dyscalculie;
 import android.icu.text.DecimalFormat;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class PercentenFragment extends Fragment {
+
+    private TextToSpeech textToSpeech;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_percenten, container, false);
         final Button button = view.findViewById(R.id.buttonCalculate);
+        final EditText oPrijs = view.findViewById(R.id.oorspronkelijkePrijs);
+        final EditText percentage = view.findViewById(R.id.percentage);
+        textToSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status){
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(new Locale("nl_NL"));
+                }
+            }
+        });
+
+        oPrijs.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                textToSpeech.speak(oPrijs.getText().toString() + " euro", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        percentage.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                textToSpeech.speak(percentage.getText().toString() + " procent", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener(){
             @RequiresApi(api = Build.VERSION_CODES.N)
             public void onClick(View v){
@@ -37,10 +67,11 @@ public class PercentenFragment extends Fragment {
         float percentage = Float.parseFloat(tPercentage.getText().toString());
         float nieuwePrijs = oorspronkelijkePrijs * (1 - (percentage / 100));
 
-        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
 
 
         tNieuwePrijs.setText(decimalFormat.format(nieuwePrijs));
+        textToSpeech.speak(decimalFormat.format(nieuwePrijs) + " euro", TextToSpeech.QUEUE_FLUSH, null);
 
     }
 }
