1f3fbc83e71228c2cd6146874207de6357692fb1
Unknown <pieterjan.dergent@gmail.com>
2017-12-17 09:51:08 +0100

    Theme fragment
    Deleted code in commentary

--- app/src/main/java/be/thomasmore/dyscalculie/ThemesFragment.java
+++ app/src/main/java/be/thomasmore/dyscalculie/ThemesFragment.java
@@ -24,29 +24,6 @@ public class ThemesFragment extends Fragment {
         Utils.onActivityCreateSetTheme(getActivity());
 
 
-
-        /*final RadioGroup radio = (RadioGroup) view.findViewById(R.id.radiogroup_themes);
-        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                View radioButton = radio.findViewById(checkedId);
-                int index = radio.indexOfChild(radioButton);
-
-                // Add logic here
-
-                switch (index) {
-                    case 0: // first button
-                        Utils.changeToTheme(getActivity(), Utils.THEME_BLACKWHITE);
-                        break;
-                    case 1: // secondbutton
-                        Utils.changeToTheme(getActivity(), Utils.THEME_PASTEL);
-                        break;
-                }
-            }
-        });*/
-
         // Inflate the layout for this fragment
         return view;
     }
